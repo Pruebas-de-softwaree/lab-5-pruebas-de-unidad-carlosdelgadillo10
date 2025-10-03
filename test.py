864f301aa32b4806d4bb1f6b1980622c76cff621
carlosdelgadillo10 <103958810+carlosdelgadillo10@users.noreply.github.com>
2025-10-03 02:40:45 +0000

    TC-Book03

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from book import Book
-
-libro = Book("hola", "George Orwell", 1949, "1234567890")
+#TC1
+libro = Book("libro6", "George Orwell", 1949, "1234567890")
 print(libro.title)
+#TC2
 libro.borrow()
+print(libro.borrowed)
+#TC3
+libro.return_book()
 print(libro.borrowed)
